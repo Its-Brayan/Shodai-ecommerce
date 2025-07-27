bec66f6134abcbd94c61b840a647b5ea26943d07
Brayan <brayanmwangi732@gmail.com>
2025-07-27 19:10:41 +0300

    orders categories and customers page

---
+++ shodai_shopify/src/components/HeaderComponent.vue
@@ -0,0 +1,45 @@
+<template>
+     <v-card-title class="pt-5">
+     <v-row>
+         
+            <v-col cols="8">
+            <v-text-field
+            variant="outlined"
+            prepend-inner-icon="mdi-magnify"
+            width="500px"
+            rounded="pill"
+            label="Search..."
+            density="compact"
+            color="grey-darken-2"
+            >
+
+            </v-text-field>
+            </v-col>
+        
+            <div class="d-flex justify-end ml-16">
+                <v-col>
+                    <v-icon style="cursor: pointer;" size="small">mdi-message-badge-outline</v-icon>
+                </v-col>
+                <v-col>
+                    <v-icon style="cursor: pointer;" size="small">mdi-bell-badge-outline</v-icon>
+                </v-col>
+                <v-col  >
+                    <v-img
+                    :width="35"
+                     aspect-ratio="16/9"
+                    src="https://cdn-icons-png.flaticon.com/128/1999/1999625.png"
+                    ></v-img>
+                </v-col>
+                <v-col>
+                   
+                    <p class="text-subtitle-1 mt-1">Brayan mwangi</p>
+                   
+                </v-col>
+                <v-col class="ml-n5" >
+                    <v-icon style="cursor: pointer;" size="small">mdi-menu-down</v-icon>
+                </v-col>
+                </div>
+            </v-row>
+          
+        </v-card-title>
+</template>
--- shodai_shopify/src/views/Pages/Tools/ProductsView.vue
+++ shodai_shopify/src/views/Pages/Tools/ProductsView.vue
@@ -1,50 +1,7 @@
 <template>
     <v-card>
         
-        <v-card-title class="pt-5">
-            
-             
-           <v-row>
-            <v-col cols="8">
-            <v-text-field
-            variant="outlined"
-            prepend-inner-icon="mdi-magnify"
-            width="500px"
-            rounded="pill"
-            label="Search Products"
-            density="compact"
-            color="grey-darken-2"
-            >
-
-            </v-text-field>
-            </v-col>
-        
-            <div class="d-flex justify-end ml-16">
-                <v-col>
-                    <v-icon style="cursor: pointer;" size="small">mdi-message-badge-outline</v-icon>
-                </v-col>
-                <v-col>
-                    <v-icon style="cursor: pointer;" size="small">mdi-bell-badge-outline</v-icon>
-                </v-col>
-                <v-col  >
-                    <v-img
-                    :width="35"
-                     aspect-ratio="16/9"
-                    src="https://cdn-icons-png.flaticon.com/128/1999/1999625.png"
-                    ></v-img>
-                </v-col>
-                <v-col>
-                   
-                    <p class="text-subtitle-1 mt-1">Brayan mwangi</p>
-                   
-                </v-col>
-                <v-col class="ml-n5" >
-                    <v-icon style="cursor: pointer;" size="small">mdi-menu-down</v-icon>
-                </v-col>
-                </div>
-            </v-row>
-          
-        </v-card-title>
+        <HeaderComponent/>
         <v-card-text class="bg-grey-lighten-4">
          <v-card class=" mt-16" >
             <v-card-text>
@@ -156,7 +113,12 @@
     :search="search"
     item-value="name"
     @update:options="loadItems"
-  ></v-data-table-server>
+  >
+  <template v-slot:item.actions="{ item,value }">
+   <v-btn prepend-icon="mdi-pencil" variant="outlined" size="small" class="text-capitalize bg-indigo ma-3">Edit</v-btn>
+    <v-icon size="small">mdi-trash-can-outline</v-icon>
+  </template>
+</v-data-table-server>
             </v-card-text>
           </v-card>
 
@@ -165,8 +127,9 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
 
+  import { ref } from 'vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
   const desserts = [
     {
       name: 'Frozen Yogurt',
@@ -273,17 +236,12 @@
   }
   const itemsPerPage = ref(5)
   const headers = ref([
-    {
-      title: 'Dessert (100g serving)',
-      align: 'start',
-      sortable: false,
-      key: 'name',
-    },
-    { title: 'Calories', key: 'calories', align: 'end' },
-    { title: 'Fat (g)', key: 'fat', align: 'end' },
-    { title: 'Carbs (g)', key: 'carbs', align: 'end' },
-    { title: 'Protein (g)', key: 'protein', align: 'end' },
-    { title: 'Iron (%)', key: 'iron', align: 'end' },
+   
+    { title: 'Product name', key: 'calories', align: 'start' },
+    { title: 'Purchase Unit Price', key: 'fat', align: 'start' },
+    { title: 'Products', key: 'carbs', align: 'start' },
+    { title: 'Status', key: 'protein', align: 'start' },
+    { title: 'actions',key:'actions', align: 'start' },
   ])
   const search = ref('')
   const serverItems = ref([])
